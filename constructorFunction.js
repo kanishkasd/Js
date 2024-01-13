91c30b0573baf743d8e8ba46d825c98472d3866b
kanishkasd <kanishkasandaru2048@gmail.com>
2024-01-13 21:24:27 +0530

    this keyword

--- constructorFunction.js
+++ constructorFunction.js
@@ -1,2 +1,24 @@
 // Job is a constructor function to create an object
 // We use Pascal notation for factory functions (OneTwo)
+
+// Factory function 
+function createCircle(radius, location){
+    return{
+        radius,  // radius: radius
+        location,
+        draw(){ // we drop the function keyword when adding a method to an object
+            console.log('draw');
+        }
+    }
+}
+
+// Constructor function
+
+function Circle(radius){
+    this.radius = radius; // set the radius property of this object to the radius argument
+    this.draw = function(){ // this is a method of the object
+        console.log('draw');
+    }
+}
+
+const circle = new Circle(1); // new operator creates an empty object, like const x = {} and then sets this to point to that object
--- this.js
+++ this.js
@@ -13,4 +13,32 @@ const video = {
     
 }
 
+function playVideo(){
+    console.log(this);
+}
+
+function Video(title){ // Pascal notation for constructor functions
+    this.title = title;
+    console.log(this);
+}
+
+
 video.stop()  // this keyword reference to object that is executing this method
+playVideo(); // this keyword reference to global object (window in browser)
+
+const v = new Video('b') // this keyword reference to new object that is created by new operator
+
+
+// 2nd example
+
+const video2 = {
+    title : 'a',
+    tags : ['a', 'b', 'c'],
+    showTags(){
+        this.tags.forEach(function(tag){
+            console.log(this.title, tag); // this is a reference to the global object because its inside a regular function
+        }, this) // we can pass this as a second argument to forEach method
+    }
+}
+
+video2.showTags();
